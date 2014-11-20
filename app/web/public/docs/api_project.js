522c24cecbe6da55fe448345bf99d65b4455c124
drei01 <mcreidie@hotmail.com>
2014-11-20 14:50:36 +0000

    add descriptions for seo

--- app/web/public/docs/api_project.js
+++ app/web/public/docs/api_project.js
@@ -5,7 +5,7 @@ define({
   "title": "Cron As A Service API",
   "url": "http://www.cronasaservice.com/api",
   "generator": {
-    "version": "0.7.2",
-    "time": "2014-10-08T21:27:10.751Z"
+    "version": "0.7.1",
+    "time": "2014-11-20T14:50:15.292Z"
   }
 });
