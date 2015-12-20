738d2e0224523cff180e7305e5d1fa21396ca794
drei01 <mcreidie@hotmail.com>
2015-12-20 09:36:44 +0000

    Update new post script to add tags and correct author

--- blog_source/public/newpost.js
+++ blog_source/public/newpost.js
@@ -14,6 +14,7 @@ var slug = require('slug');
 var fs = require('fs');
 var file = './_data.json';
 var config;
+var harpConfig = require('../harp.json');
 
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
@@ -31,45 +32,47 @@ fs.readFile(file, 'utf8', function (err, data) {
   rl.question("Title: ", function(title) {
     rl.question("Description: ", function(description) {
       rl.question("Facebook Thumbnail Image URL: ", function(fbImage) {
-        var author = "admin";
-        var ptype = "post";
-        var status = "publish";
+        rl.question("Tags: ", function(tags){
+            var author = harpConfig.author;
+            var ptype = "post";
+            var status = "publish";
 
-        // slug module doesn't replace periods which causes Harp to choke when serving up static file
-        var slugTitle = slug(title).toLowerCase().split('.').join("");
-        var publishDate = (new Date).getTime();
+            // slug module doesn't replace periods which causes Harp to choke when serving up static file
+            var slugTitle = slug(title).toLowerCase().split('.').join("");
+            var publishDate = (new Date).getTime();
 
-        var maxProp = "ID";
-        var maxVal = 0;
+            var maxProp = "ID";
+            var maxVal = 0;
 
-        for (post in config) {
-            var value = parseInt(config[post][maxProp], 10);
-            if (value > maxVal) {
-                maxVal = value;
+            for (post in config) {
+                var value = parseInt(config[post][maxProp], 10);
+                if (value > maxVal) {
+                    maxVal = value;
+                }
             }
-        }
 
-        var id = maxVal + 1;
+            var id = maxVal + 1;
 
-        var newPost = {"ID": id, "author": author, "date": publishDate, "ptype": ptype, "description": description, "slug": slugTitle, "status": status, "title": title, "FBImage": fbImage};
+            var newPost = {"ID": id, "author": author, "date": publishDate, "ptype": ptype, "description": description, "slug": slugTitle, "status": status, "title": title, "FBImage": fbImage, "tags": tags.split(',')};
 
-        config[slugTitle] = newPost;
+            config[slugTitle] = newPost;
 
-        fs.writeFile(file, JSON.stringify(config, null,  2), function(err) {
-          if(err) {
-            console.log(err);
-          } else {
-          fs.writeFile("./"+slugTitle+".md", description, function(err) {
-            if(err) {
-              console.log(err);
-            } else {
-              console.log("The new post was created as ../"+slugTitle+".md");
-            }
-          }); 
-          console.log("_data.json was updated");
-          }
-        }); 
-        rl.close();
+            fs.writeFile(file, JSON.stringify(config, null,  2), function(err) {
+              if(err) {
+                console.log(err);
+              } else {
+              fs.writeFile("./"+slugTitle+".md", description, function(err) {
+                if(err) {
+                  console.log(err);
+                } else {
+                  console.log("The new post was created as ../"+slugTitle+".md");
+                }
+              }); 
+              console.log("_data.json was updated");
+              }
+            }); 
+            rl.close();
+        });
       });     
     });
   });
