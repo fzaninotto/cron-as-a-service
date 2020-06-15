6cac99c06a4521fb33329aa4e994f1b4ba415b63
Rodrigo García <chicotec.asm@gmail.com>
2020-06-15 06:50:55 -0500

    fix: update project to express 4

--- app.js
+++ app.js
@@ -1,56 +1,64 @@
 /*
  * Remote cron service
  */
-var path     = require('path');
+var fs     = require('fs');
 var config   = require('config');
 var mongoose = require('mongoose');
 var express  = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler = require('errorhandler');
+var api = require('./app/api');
 
 var CronTab = require('./lib/cronTab');
 var Job     = require('./models/job');
 
 // configure mongodb
-mongoose.connect('mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.server +'/' + config.mongodb.database);
+mongoose.connect('mongodb://' + config.mongodb.user + ':' + config.mongodb.password + '@' + config.mongodb.server + ':' + config.mongodb.port + '/' + config.mongodb.database + '?authSource=' + config.mongodb.authSource);
 mongoose.connection.on('error', function (err) {
   console.error('MongoDB error: ' + err.message);
   console.error('Make sure a mongoDB server is running and accessible by this application');
   process.exit();
 });
 
-var app = module.exports = express.createServer();
+var app = express();
 
-app.configure(function(){
-  app.use(app.router);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.configure('development', function() {
+// parse application/json
+app.use(bodyParser.json())
+
+// override with the X-HTTP-Method-Override header in the request
+app.use(methodOverride('X-HTTP-Method-Override'))
+
+if (process.env.NODE_ENV === 'development') {
   if (config.verbose) mongoose.set('debug', true);
   app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+}
 
-app.configure('production', function() {
+if (process.env.NODE_ENV === 'production') {
   var oneYear = 31557600000;
   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
-});
+  app.use(errorHandler());
+}
 
 // initialize jobs
 var nbInitializedJobs = 0;
-Job.find({}).each(function(err, job) {
+Job.find({}, function(err, docs) {
   if (err) return callback(err);
-  if (job) {
+  docs.forEach(job => {
     CronTab.add(job);
     nbInitializedJobs++;
     console.log('Initialized job %s (target %s on %s)', job._id, job.url, job.expression);
+  });
+
+  if (nbInitializedJobs) {
+    console.log('Initialization complete. %d jobs initialized.', nbInitializedJobs);
   } else {
-    if (nbInitializedJobs) {
-      console.log('Initialization complete. %d jobs initialized.', nbInitializedJobs);
-    } else {
-      console.log('Starting with empty job collection.');
-    }
+    console.log('Starting with empty job collection.');
   }
 });
 
@@ -62,15 +70,17 @@ process.on('exit', function () {
 });
 
 // Routes
-app.use('/api', require('./app/api'));
+app.use('/api', api);
 app.use('/web', require('./app/web'));
 
 // load plugins
-path.exists('./plugins/index.js', function(exists) {
+fs.exists('./plugins/index.js', function(exists) {
   if (exists) {
     require('./plugins').init(app, config, mongoose);
   };
 });
 
-app.listen(config.server.port);
-console.log("Express server listening on port %d in %s mode", config.server.port, app.settings.env);
+var server = http.createServer(app)
+server.listen(config.server.port, function () {
+  console.log("Express server listening on port %d in %s mode", config.server.port, app.settings.env);
+})
--- app/web/index.js
+++ app/web/index.js
@@ -1,24 +1,28 @@
 /**
  * Module dependencies.
  */
-var express    = require('express');
-var app = module.exports = express.createServer();
+var errorHandler = require('errorhandler');
+var express      = require('express');
+var http         = require('http');
+
+var app = express();
 
 // middleware
 
-app.configure(function(){
-  app.use(app.router);
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.static(__dirname + '/public'));
-});
+app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res, next) {
   res.render('index', { route: app.route });
 });
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log('Express started on port 3000');
+  var server = http.createServer(app)
+  server.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'))
+  })
 }
+
+module.exports = app;
